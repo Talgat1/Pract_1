9345a04f6d09c376d4c6c79edc16f96a8b995e14
arullintalgat36@gmail.com <arullintalgat36@gmail.com>
2022-01-20 22:35:19 +0300

    Добавьте файлы проекта.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Reg(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PageFirst());
+        }
+        private async void Vhod(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PageSecond());
+        }
+    }
+}
---
+++ App1/App1/PageSecond.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageSecond : ContentPage
+    {
+        public PageSecond()
+        {
+            InitializeComponent();
+        }
+        private void project_List(object sender, ItemTappedEventArgs e)
+        {
+            Navigation.PushAsync(new PageThird());
+        }
+    }
+}
